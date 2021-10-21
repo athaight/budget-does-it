9aad4c613dcbae32597f9b049146fa71066cecc7
Andrew Haight <haight.haight@gmail.com>
2021-10-21 12:32:18 -0700

    service worker started

---
+++ public/service-worker.js
@@ -0,0 +1,12 @@
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/manifest.webmanifest",
+  "/assets/css/style.css",
+  "/assets/js/index.js",
+  "/assets/icons/icon-192x192.png",
+  "/assets/icons/icon-512x512.png"
+];
+
+const CACHE_NAME = "static-assets-cache";
+const DATA_CACHE_NAME = "data-cache";
