cf160b0f0b23983a6553e6168f87a19f441304ea
Andrew Haight <haight.haight@gmail.com>
2021-10-27 16:14:11 -0700

    db creation and requests

---
+++ public/db.js
@@ -0,0 +1,24 @@
+let db;
+let budgetVersion;
+
+// Create a new db request for a "budget" database.
+const request = indexedDB.open('BudgetDB', budgetVersion || 21);
+
+request.onupgradeneeded = function (e) {
+  console.log('Upgrade needed in IndexDB');
+
+  const { oldVersion } = e;
+  db = e.target.result;
+  const newVersion = e.newVersion || db.version;
+
+  console.log(`DB Updated from version ${oldVersion}👴 to ${newVersion}👶`);
+
+
+  if (db.objectStoreNames.length === 0) {
+    db.createObjectStore('BudgetStore', { autoIncrement: true });
+  }
+};
+
+request.onerror = function (e) {
+  console.log(`Woops!🙊 ${e.target.errorCode}`);
+};
