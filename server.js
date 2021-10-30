803bbae861d44e81828cb05395f0de8f4260519e
Andrew Haight <haight.haight@gmail.com>
2021-10-30 12:35:06 -0700

    todo for db troubleshooting

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+// TODO THink the issue is here
 mongoose.connect("mongodb://localhost/budget", {
   useNewUrlParser: true,
   // useFindAndModify: false,
